bc7cae150fd4417025b6561d1549da9c57069308
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-04 20:23:07 +0530

    Update tsx script

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 /** @format */
 
 import express from 'express';
+import path from 'node:path';
 
 const app = express();
 
-// app.use(express.static())
+app.use(express.static(path.join(path.resolve(), '/public')));
+app.use(express.json);
 
 export default app;
