8985b2c36c7e099d339aaa76cf011a972b01ba40
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-05 15:40:47 +0530

    Add not found error handler

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ import { CustomErrorHandler } from './handlers/CustomError.handler.js';
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(express.static(path.join(path.resolve(), '/public')));
 
@@ -22,6 +23,12 @@ app.get('/', (_req: Request, res: Response, next: NextFunction) => {
     status: 'ok',
     timestamp: new Date().toISOString(),
   });
+});
+
+// 404 route
+app.use((req: Request, _res: Response, next: NextFunction) => {
+  console.log(`${req.method} ${req.path}`);
+  return next(new CustomErrorHandler(404, 'Route not found !', 'app'));
 });
 
 app.use(errorHandler);
