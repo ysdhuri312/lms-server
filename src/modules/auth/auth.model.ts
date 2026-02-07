e59a0f91038a4efe2e47b3a07e8017e94bf64b55
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-07 11:10:08 +0530

    Add auth model

---
+++ src/modules/auth/auth.model.ts
@@ -0,0 +1,18 @@
+/** @format */
+
+import mongoose from 'mongoose';
+import { type IAuth } from './auth.types.js';
+
+const authSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    refreshToken: { type: String, default: null },
+    provider: { type: String, enum: ['local', 'google'], default: 'local' },
+    lastLogin: { type: Date, required: true },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IAuth>('Auth', authSchema);
