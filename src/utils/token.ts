a59ddd627035363672c2259c0260a407e37dc3d5
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-02-12 19:14:47 +0530

    Add verify token funtion

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -2,9 +2,22 @@
 
 import jwt from 'jsonwebtoken';
 import { env } from '../config/env.js';
+import { CustomErrorHandler } from '../handlers/CustomError.js';
+import { userInfo } from 'node:os';
 
 export const generateToken = (paylod: { userId: string; role: string }) => {
   return jwt.sign(paylod, env.JWT_TOKEN_SECRET, {
     expiresIn: '7d',
   });
+};
+
+export const verifyToken = (token: string) => {
+  if (!token) throw new CustomErrorHandler(401, 'Token not available');
+
+  const decoded = jwt.verify(token, env.JWT_TOKEN_SECRET, (err, result) => {
+    if (err) throw new CustomErrorHandler(401, 'Invalid token');
+    return result;
+  });
+
+  return { user: decoded };
 };
